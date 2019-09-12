8a76f2a61d69ad43aea2c03a1bfb92b0292bdfcf
Дарья <dariakurbasova@gmail.com>
2019-09-12 16:24:36 +0300

    Оформлено окно загрузки

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
         <input class = "your-name" type="text" id="name">
         </label>
 
-        <button id="startButton">
+        <button id="startButton" class = "button">
             Начать игру
         </button>
     </div>
@@ -49,8 +49,9 @@
 
 <div id="waitingPanel" style="display: none;">
     <h2>Ожидание игры...</h2>
+    <svg xmlns:svg="http://www.w3.org/2000/svg" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.0" width="120px" height="120px" viewBox="0 0 128 128" xml:space="preserve"><g><linearGradient id="linear-gradient"><stop offset="0%" stop-color="#000"/><stop offset="100%" stop-color="#0090fe"/></linearGradient><linearGradient id="linear-gradient2"><stop offset="0%" stop-color="#000"/><stop offset="100%" stop-color="#90e6fe"/></linearGradient><path d="M64 .98A63.02 63.02 0 1 1 .98 64 63.02 63.02 0 0 1 64 .98zm0 15.76A47.26 47.26 0 1 1 16.74 64 47.26 47.26 0 0 1 64 16.74z" fill-rule="evenodd" fill="url(#linear-gradient)"/><path d="M64.12 125.54A61.54 61.54 0 1 1 125.66 64a61.54 61.54 0 0 1-61.54 61.54zm0-121.1A59.57 59.57 0 1 0 123.7 64 59.57 59.57 0 0 0 64.1 4.43zM64 115.56a51.7 51.7 0 1 1 51.7-51.7 51.7 51.7 0 0 1-51.7 51.7zM64 14.4a49.48 49.48 0 1 0 49.48 49.48A49.48 49.48 0 0 0 64 14.4z" fill-rule="evenodd" fill="url(#linear-gradient2)"/><animateTransform attributeName="transform" type="rotate" from="0 64 64" to="360 64 64" dur="1800ms" repeatCount="indefinite"></animateTransform></g></svg>
     <h3>Вы вошли как: <span id="waitingPlayerName"></span></h3>
-    <button id="cancelButton">
+    <button id="cancelButton" class = "button">
         Отмена
     </button>
 </div>
@@ -109,13 +110,13 @@
 
 
     <div class="button_center">
-        <button class="toggleColored">Вкл/выкл раскраску</button>
+        <button class="toggleColored big-button">Вкл/выкл раскраску</button>
     </div>
 
     <div class= "phase">
         <div class = "guessing">
             <p class = "glue">Подсказка: <span class="glueWordAndNumber">котики 3</span></p>
-            <button class = "stop_guessing_button">Завершить ход</button>
+            <button class = "stop_guessing_button big-button">Завершить ход</button>
         </div>
 
         <div class = "making_glue">
@@ -123,7 +124,7 @@
                 <input class = "glue_text" type = "text" name = "glue_text">
             </label>
             <input class = "glue_number" type = "number" min="1" max="9" name = "glue_number">
-            <button class = "making_glue_button">Отправить подсказку</button>
+            <button class = "making_glue_button big-button">Отправить подсказку</button>
         </div>
 
         <div class = "waiting_guessing">
@@ -145,16 +146,17 @@
         <br>
         Затрачено времени: <span class="winTimeSpent">пока не измеряется</span>
         <br>
-        <button class="restartGame">Начать заново</button>
+        <button class="restartGame button">Начать заново</button>
     </div>
 
     <div class="losePanel" style="display: none;">
         <h2>
             Вы потерпели поражение!
         </h2>
-        Игрок <span class="losePlayerName"></span> нажал на слово <span class="loseWord"></span>
+        <p style="font-size: 20px">Игрок <span class="losePlayerName"></span> нажал на слово "<span class="loseWord"></span>".
+            Для новой игры оба игрока должны нажать на кнопку и перезапустить страницу.</p>
         <br>
-        <button class="restartGame">Начать заново</button>
+        <button class="restartGame button">Начать заново</button>
     </div>
 </section>
 
